60cbd399488f16a058e3ebad89ad2aedd9ed75d2
Barış <bayazitbaris@gmail.com>
2025-05-26 16:01:43 -0400

    Complete challenge 3

--- src/two_k_15/README.md
+++ src/two_k_15/README.md
@@ -4,3 +4,16 @@
 - Translated from [Why3 team's solution](https://toccata.gitlabpages.inria.fr/toccata/gallery/verifythis2015.en.html) directly.
 - Filled in some extra proofs for Verus after directly translating from Why3 (e.g, that early return of the algorithm matches the spec; see line 49).
 - Note: There are a lot of improvements that can be made to the proof. I believe simplifying array equality (`eq_array`) with forall/subrange/etc, and as a result `spec_is_relaxed_prefix`, and the main loops' invariant are some examples. I missed out on some improvements because I translated directly from the solution.
+
+## Challenge 3
+- Taken directly from the [Why3 team's solution](https://toccata.gitlabpages.inria.fr/toccata/gallery/verifythis2015.en.html).
+- I believe the proof can be significantly improved (just like Challenge 1), especially with tweaking triggers. The additional lines (compared to the original Why3 team's proof) I added mostly invoke the trigger for `is_list`. 
+- The proof performance is not great. With a simple direct translation (without the additional proof block in remove) at the end of `remove`:
+```rs
+assert(is_list(*l, s)) by {
+    assert(forall|k: int| #![trigger s[k]]
+        0 <= k < s.len() ==> (Seq::empty().push(i as int) + s)[k + 1] == s[k]);
+}
+```
+
+the proof is verified in ~10s (Apple M4 Pro, 24GB Ram) with the `--profile` flag. The main bottleneck comes from the nested forall in `s[k] != s[ki]` of `is_list`. With the additional proof block, that is reduced, but I suspect that there could be a better way to write the triggers. So, debugging the triggers with `--profile` might be a good idea.
---
+++ src/two_k_15/challenge3.rs
@@ -0,0 +1,139 @@
+use vstd::prelude::*;
+
+verus! {
+
+pub struct Dll {
+    pub prev: Vec<i32>,
+    pub next: Vec<i32>,
+    #[allow(unused)]
+    pub ghost n: int,
+}
+
+impl Dll {
+    pub open spec fn well_formed(&self) -> bool {
+        self.prev.len() == self.next.len() == self.n >= 0
+    }
+}
+
+spec fn valid_in(l: Dll, i: int) -> bool {
+    &&& l.well_formed()
+    &&& 0 <= i < l.n
+    &&& 0 <= l.prev[i] < l.n
+    &&& 0 <= l.next[i] < l.n
+    &&& l.next[l.prev[i] as int] == i
+    &&& l.prev[l.next[i] as int] == i
+}
+
+spec fn valid_out(l: Dll, i: int) -> bool {
+    &&& l.well_formed()
+    &&& 0 <= i < l.n
+    &&& 0 <= l.prev[i] < l.n
+    &&& 0 <= l.next[i] < l.n
+    &&& l.next[l.prev[i] as int] == l.next[i]
+    &&& l.prev[l.next[i] as int] == l.prev[i]
+}
+
+pub open spec fn is_list(l: Dll, s: Seq<int>) -> bool {
+    forall|k: int|
+        #![trigger s[k]]
+        #![trigger l.prev[s[k]]]
+        0 <= k < s.len() ==> (0 <= s[k] < l.n && l.prev[s[k]] == s[if k == 0 {
+            s.len() - 1
+        } else {
+            k - 1
+        }] && l.next[s[k]] == s[if k == s.len() - 1 {
+            0
+        } else {
+            k + 1
+        }] && (forall|ki: int| 0 <= ki < s.len() ==> k != ki ==> s[k] != s[ki]))
+}
+
+// Challenge 3
+// See README to better understand why I added the proof at the bottom (I believe the proof
+// can be written better)
+#[allow(unused)]
+exec fn remove(l: &mut Dll, i: i32, Ghost(s): Ghost<Seq<int>>)
+    requires
+        old(l).well_formed(),
+        valid_in(*old(l), i as int),
+        is_list(*old(l), Seq::empty().push(i as int) + s),
+    ensures
+        valid_out(*l, i as int),
+        l.well_formed(),
+        is_list(*l, s),
+{
+    l.prev[l.next[i as usize] as usize] = l.prev[i as usize];
+    l.next[l.prev[i as usize] as usize] = l.next[i as usize];
+
+    assert(is_list(*l, s)) by {
+        let s_cons = Seq::empty().push(i as int) + s;
+
+        if s.len() == 0 {
+            // Simple
+        } else {  // s.len() > 0
+            // rlimit?
+            let _ = old(l).prev[i as int];
+            let _ = old(l).next[i as int];
+
+            assert(old(l).next[i as int] == s[0]) by {
+                assert(s_cons[1] == s[0]);
+            }
+
+            assert forall|k: int| #![trigger s[k]] (0 <= k < s.len()) implies ((0 <= s[k] < l.n
+                && l.prev[s[k]] == s[if k == 0 {
+                s.len() - 1
+            } else {
+                k - 1
+            }] && l.next[s[k]] == s[if k == s.len() - 1 {
+                0
+            } else {
+                k + 1
+            }] && (forall|ki: int| 0 <= ki < s.len() ==> k != ki ==> s[k] != s[ki]))) by {
+                let current_node = s[k];
+                // r limit?
+                let _ = s[if k == 0 {
+                    s.len() - 1
+                } else {
+                    k - 1
+                }];
+
+                assert forall|ki: int| 0 <= ki < s.len() && k != ki implies current_node
+                    != s[ki] by {
+                    if (0 <= k < s.len()) {
+                        let s_f_k_trigger = (k + 1) as int;
+                        let s_f_ki_trigger = (ki + 1) as int;
+                        assert(s_cons[(s_f_k_trigger)] != s_cons[s_f_ki_trigger]);
+                    }
+                }
+
+                assert(old(l).prev[s_cons[(k + 1) as int]] == s_cons[k as int]);
+            };
+        }
+    }
+
+    // Trigger problems? This is the simplest direct translation from the Why3 solution.
+    // assert(is_list(*l, s)) by {
+    //     assert(forall|k: int| #![trigger s[k]]
+    //         0 <= k < s.len() ==> (Seq::empty().push(i as int) + s)[k + 1] == s[k]);
+    // }
+}
+
+#[allow(unused)]
+exec fn put_back(l: &mut Dll, i: i32, Ghost(s): Ghost<Seq<int>>)
+    requires
+        old(l).well_formed(),
+        valid_out(*old(l), i as int),
+        is_list(*old(l), s),
+        0 < s.len(),
+        old(l).next[i as int] == s[0],
+        s[0] != i,
+    ensures
+        l.well_formed(),
+        valid_in(*l, i as int),
+        is_list(*l, Seq::empty().push(i as int) + s),
+{
+    l.prev[l.next[i as usize] as usize] = i;
+    l.next[l.prev[i as usize] as usize] = i;
+}
+
+} // verus!
